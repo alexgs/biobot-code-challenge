8fd49e4f697c8d77bf605ab5773e994b8df0d797
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-14 12:25:26 -0400

    Add input field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,38 @@
-import { Container } from '@mantine/core';
+import { Container, TextInput, Title } from '@mantine/core';
+import { useDebouncedValue } from '@mantine/hooks';
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [searchValue, setSearchValue] = React.useState('');
+  const [debounced] = useDebouncedValue(searchValue, 250);
+
+  function handleLabelChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchValue(e.currentTarget.value);
+  }
+
+  React.useEffect(() => {
+    console.log(debounced);
+  }, [debounced]);
+
   return (
     <>
       <Head>
-        <title>Biobot Code Challenge</title>
+        <title>Biobot Label Tracking</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container size="xs" px="xs" pt="md" className={inter.className}>
-        xs container with xs horizontal padding
+        <Title order={1}>Biobot Label Tracking</Title>
+        <TextInput
+          label="Please enter your label ID to get started"
+          onChange={handleLabelChange}
+          placeholder="Label ID"
+          withAsterisk
+        />
       </Container>
     </>
   );
